bb219356093dfa29bfef7d39803b19c2b32474b3
krdx <appdev.krdx@gmail.com>
2024-02-08 00:30:43 +0530

    GTK Setup

---
+++ build/CMakeFiles/TceLib.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TceLib.dir/main.c.obj"
+  "CMakeFiles/TceLib.dir/main.c.obj.d"
+  "TceLib.exe"
+  "TceLib.exe.manifest"
+  "TceLib.pdb"
+  "libTceLib.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TceLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/TceLib.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TceLib.
+# This may be replaced when dependencies are built.
